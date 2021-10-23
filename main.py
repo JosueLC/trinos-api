08864e239f6b98bd7c75e90c259a5e7a75aac54a
JosueLC <josueflopez@msn.com>
2021-10-23 10:19:28 -0500

    path operations

--- main.py
+++ main.py
@@ -11,27 +11,6 @@ from paths import user, trino
 
 app = FastAPI()
 
-#Path operations to home page
-@app.get(
-    path="/",
-    status_code=status.HTTP_200_OK,
-    summary="Home page",
-    tags=["Home"]
-    )
-def home():
-    """
-    Home
-
-    This endpoint returns the home page.
-
-    Parameters:
-    None
-
-    Returns:
-    Dictionary with the message "Working!".
-    """
-    return {"Trinos API": "Working!"}
-
 #Includes the paths from paths folder
 app.include_router(user.router)
 app.include_router(trino.router)
--- paths/trino.py
+++ paths/trino.py
@@ -6,7 +6,69 @@ from fastapi import APIRouter
 from fastapi import status
 
 #Local packages
-#from models.user import User
+from models.trino import Trino
 
 router = APIRouter()
+
+#Path operations to home page
+@router.get(
+    path="/",
+    response_model=List[Trino],
+    status_code=status.HTTP_200_OK,
+    summary="Get all trinos",
+    tags=["Home","Trino"]
+    )
+def home():
+    """
+    Home
+
+    This endpoint returns all trinos.
+
+    Parameters:
+    None
+
+    Returns:
+    Dictionary with the message "Working!".
+    """
+    return list()
+
+@router.get(
+    path="/trino/{trino_id}",
+    response_model=Trino,
+    status_code=status.HTTP_200_OK,
+    summary="Get trino by id",
+    tags=["Trino"]
+    )
+def get_trino(trino_id: int):
+    pass
+
+@router.post(
+    path="/trino",
+    response_model=Trino,
+    status_code=status.HTTP_201_CREATED,
+    summary="Post a trino",
+    tags=["Trino"]
+    )
+def post_trino(trino: Trino):
+    pass
+
+@router.put(
+    path="/trino/{trino_id}",
+    response_model=Trino,
+    status_code=status.HTTP_200_OK,
+    summary="Update a trino",
+    tags=["Trino"]
+    )
+def update_trino(trino_id: int, trino: Trino):
+    pass
+
+@router.delete(
+    path="/trino/{trino_id}",
+    response_model=Trino,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a trino",
+    tags=["Trino"]
+    )
+def delete_trino(trino_id: int):
+    pass
 
