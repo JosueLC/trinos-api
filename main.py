06024d49301636a63d219487f4f365b784902bc7
JosueLC <josueflopez@msn.com>
2021-10-22 20:35:56 -0500

    fastapi working

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+#Python packages
 
+#Pydantic packages
 
-if __name__ == "__main__":
-    print("Hi")
+#FastAPI packages
+from fastapi import FastAPI
+from fastapi import status
+
+app = FastAPI()
+
+#Path operations to home page
+@app.get(
+    path="/",
+    status_code=status.HTTP_200_OK,
+    summary="Home page",
+    )
+def home():
+    """
+    Home
+
+    This endpoint returns the home page.
+
+    Parameters:
+    None
+
+    Returns:
+    Dictionary with the message "Working!".
+    """
+    return {"Trinos API": "Working!"}
